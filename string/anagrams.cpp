07ffced92b36afbfdd924363f81b2a53fcc2f8ba
aksharanigam1112 <aksharanigam111298@gmail.com>
2020-07-26 20:01:26 +0530

    Subseq match, min search cost, anagrams

---
+++ string/anagrams.cpp
@@ -0,0 +1,16 @@
+// Create all possible anagrams of a given word
+#include<iostream>
+#include<string>
+using namespace std;
+
+void anagrams(string word)
+{
+    
+}
+
+int main()
+{
+    string word = "EAT";
+    cout<<"Possible Anagrams are : "<<endl;
+    anagrams(word);
+}
