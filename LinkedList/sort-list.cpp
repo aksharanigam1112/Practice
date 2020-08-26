2467468215ea4757033366ab3d90a00a557d47b3
aksharanigam1112 <aksharanigam111298@gmail.com>
2020-08-26 22:43:25 +0530

    Sort Linked List using Merge Sort

--- LinkedList/sort-list.cpp
+++ LinkedList/sort-list.cpp
@@ -1,4 +1,4 @@
-// Sort a linked list in O(n log n) time using constant space complexity.
+// Sort a linked list using constant space complexity.
 #include<iostream>
 using namespace std;
 struct ll
@@ -27,7 +27,7 @@ void print(ll*head)
 
 
 // TC : O(N^2)
-ll* sortList(ll*root)
+ll* sortList1(ll*root)
 {
     if(root == NULL)
         return NULL;
@@ -60,10 +60,58 @@ ll* sortList(ll*root)
     return newhead;
 }
 
+// Merging two sorted lists 
+ll* mergeTwoLists(ll* l1, ll* l2) 
+{
+    if(l1 == NULL)
+        return l2;
+    
+    if(l2 == NULL)
+        return l1;
+        
+    ll* newhead;
+        
+    if(l1->data <= l2->data)
+    {
+        newhead = l1;
+        newhead->next = mergeTwoLists(l1->next,l2);
+    }
+    else
+    {
+        newhead = l2;
+        newhead->next = mergeTwoLists(l1,l2->next);
+    }
+    return newhead;
+}
+    
+// Using MergeSort Technique TC : O(NlogN)
+
+ll* sortList2(ll* head) 
+{
+    if(head == NULL || head->next == NULL)
+		return head;
+
+    ll *slow = head, *fast = head->next;
+
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+        
+    ll* head2 = slow->next;
+    slow->next = NULL;
+
+    ll* l1 = sortList2(head);
+    ll* l2 = sortList2(head2);
+
+    return mergeTwoLists(l1, l2);
+}
+
 int main()
 {
     ll *root = newNode(-1);
-    root->next = newNode(3);
+    root->next = newNode(5);
     root->next->next = newNode(3);
     root->next->next->next = newNode(4);
     root->next->next->next->next = newNode(0);
@@ -71,7 +119,7 @@ int main()
     cout<<"Original List : ";
     print(root);
 
-    ll *sorted = sortList(root);
+    ll *sorted = sortList2(root);
     cout<<"Sorted List : ";
     print(sorted);
 }
