d1674f31cb22814c8c1050dd0828071552b59fc3
aksharanigam1112 <aksharanigam111298@gmail.com>
2020-08-31 20:32:17 +0530

    BT Colouring game

--- bt-bst/bt-colouring-game.cpp
+++ bt-bst/bt-colouring-game.cpp
@@ -1,4 +1,10 @@
+// We are given the root of this binary tree, and the number of nodes n in the tree.  
+// n is odd, and each node has a distinct value from 1 to n.
+// Player 1 ==> x ==> Red
+// Player 2 ==> y ==> Blue
+// If there exists a value of y for which player 2 always win return true else false 
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 struct node
 {
@@ -13,43 +19,30 @@ node* newNode(int data)
     n->left = n->right = NULL;
 }
 
-int countNode(node*root)
+int dfs(node *root,vector<int>&dp,int &x,int &l,int &r)
 {
-    if(root==NULL)
-        return 0;
-    return 1 + countNode(root->left) + countNode(root->right);
-}
+    if(root->left)
+        dp[root->data]+=dfs(root->left,dp,x,l,r);
+    if(root->right)
+        dp[root->data]+=dfs(root->right,dp,x,l,r);
 
-void findNode(node* root, int x, int &l, int &r)
-{
-    if(root==NULL)
-        return ;
-    if(root->data == x)
+    if(root->data==x)
     {
-        l = countNode(root->left);
-        r = countNode(root->right);
-        return;
+        l = root->left ? dp[root->left->data]:0;
+        r = root->right ? dp[root->right->data]:0;
     }
-    findNode(root->left,x,l,r);
-    findNode(root->right,x,l,r);
+    return ++dp[root->data];
 }
 
-bool colourTree(node*root, int n, int x)
+bool colourBT(node* root, int n, int x) 
 {
-    if(n==1)
-        return false;
-    int l=0,r=0;
-
-    findNode(root,x,l,r);
+    vector<int>dp(n+1);
+    int l,r;
     
-    // left child is considered when number of nodes in left subtree of x are > n/2.
-    // OR right child is considered when number of nodes in right subtree of x are > n/2
-    if(l>n/2 || r>n/2)
+    dfs(root,dp,x,l,r);
+    if(max(n-dp[x],max(l,r))>n/2)
         return true;
-
-    //  parent of x is considered for y
-    // when number in nodes in tree of x < (n+1)/2 
-    return (l+r+1 < n+1/2);
+    return false;
 }
 
 int main()
@@ -66,5 +59,5 @@ int main()
     root->left->left->left = newNode(6);
     root->left->left->right = newNode(7);
 
-    cout<<"Does player 2 win ? "<<(colourTree(root,8,3) ? "Yes" : "No")<<endl;
+    cout<<"Does player 2 win ? "<<(colourBT(root,8,3) ? "Yes" : "No")<<endl;
 }
