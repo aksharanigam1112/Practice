e437313156793eb2c02c6f82b8554d564ef631d1
aksharanigam1112 <aksharanigam111298@gmail.com>
2020-09-08 21:26:23 +0530

    Add one row to the bst

---
+++ bt-bst/add-one-rwo.cpp
@@ -0,0 +1,76 @@
+// Given the root of a binary tree, then value v and depth d, 
+// you need to add a row of nodes with value v at the given depth d. 
+// The root node is at depth 1. 
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node*left,*right;
+};
+node* newNode(int data)
+{
+    node* n = new node;
+    n->data = data;
+    n->left = n->right = NULL;
+    return n;
+}
+void print(node*root)
+{
+    if(root==NULL)
+        return ;
+    
+    cout<<root->data<<" ";
+    print(root->left);
+    print(root->right);
+}
+
+node* addRow(node*root,int v, int d, int curr_depth)
+{
+    if(root==NULL)
+        return root;
+    
+    if(d==1)
+    {
+        node* newroot = newNode(v);
+        newroot->left = root;
+        return newroot;
+    }
+
+    else if(curr_depth == d-1)
+    {
+        node* lv = root->left;
+        node* rv = root->right;
+        root->left = newNode(v);
+        root->right = newNode(v);
+
+        root->left->left = lv;
+        root->right->right = rv;
+        return root;
+    }
+
+    root->left = addRow(root->left,v,d,curr_depth+1);
+    root->right = addRow(root->right,v,d,curr_depth+1);
+    return root;
+}
+
+int main()
+{
+    node* root = newNode(4);
+    root->left = newNode(2);
+    root->right = newNode(6);
+
+    root->left->left = newNode(3);
+    root->left->right = newNode(1);
+    root->right->left = newNode(5);
+
+    cout<<"Original tree : ";
+    print(root);
+    cout<<endl;
+
+    node* newTree = addRow(root,1,3,1);
+    cout<<"After adding row : ";
+    print(newTree);
+    cout<<endl;
+
+}
