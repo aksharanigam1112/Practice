96ee8e9ef32bf1281e474b60eadcb4a4b0451225
aksharanigam1112 <aksharanigam111298@gmail.com>
2020-09-20 19:23:45 +0530

    Peak valley in array

--- array/peak-valley.cpp
+++ array/peak-valley.cpp
@@ -1,3 +1,35 @@
 // Alterrnate peak valley arrangement  of the array
-// 1) Sort & swap alternate elemets
-// 2) Partitioning 
+// Even Pos : Greater Element
+// Odd Pos : Smaller Element
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+// Sort & swap alternate elements  TC : O(N LogN)
+
+// TC : O(N)
+void peakValley(int arr[], int size)
+{
+    for(int i=1;i<size;i+=2)
+    {
+        if(arr[i-1] > arr[i])
+            swap(arr[i],arr[i-1]);
+        
+        if(i+1<size && arr[i+1] > arr[i])
+            swap(arr[i],arr[i+1]);
+    }
+
+    // Printing the array
+    for(int i=0;i<size;i++)
+        cout<<arr[i]<<" ";
+    
+    cout<<endl;
+}
+
+int main()
+{
+    int arr[] = {6,9,2,5,4,1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Peak Valley array : ";
+    peakValley(arr,size);
+}
