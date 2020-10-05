6786fdbb11c4aae5780adff3ed340b54a43f7da3
aksharanigam1112 <aksharanigam111298@gmail.com>
2020-10-05 20:36:45 +0530

    Count Smaller Nos After itself

--- array/count-smaller-after.cpp
+++ array/count-smaller-after.cpp
@@ -6,43 +6,29 @@ using namespace std;
 // Brute Force TC : O(N^2)  &   SC : O(1)
 
 // TC : O(N)   &   SC : O(N)
-vector<int> countSmaller(int arr[], int n)
-{
-    deque<pair<int,int>>d;
-    vector<int>ans(n,0);
+vector<int> countSmallerRight(int arr[], int len) 
+{ 
+	multiset<int> s; 
+	vector<int>countSmaller(len); 
     
-    d.push_front({arr[n-1],0});
-    
-    for(int i=n-2;i>=0;i--)
-    {
-        if(arr[i] >= d.front().first)
-        {
-            int max_val=-1,count=0;
-            while(!d.empty() && arr[i] > d.front().first)
-            {
-                max_val = max(max_val,d.front().second);
-                d.pop_front();
-                count++;
-            }
-            
-            d.push_front({arr[i],max(max_val+1,count)});
-            ans[i] = max(max_val+1,count);
-        }
-        else
-        {
-            d.push_front({arr[i],d.front().second});
-            ans[i] =  d.front().second;
-        }
-    }
-    return ans;
-}
+	for (int i=len-1; i>=0; i--) 
+    { 
+		s.insert(arr[i]); 
+        // Find the first element that is lower than arr[i]
+		auto it = s.lower_bound(arr[i]); 
+        // Find the distance between above found element and the beginning of the set
+        // & store the distance 
+		countSmaller[i] = distance(s.begin(), it); 
+	}
+    return countSmaller; 
+} 
 
 int main()
 {
-    int arr[] = {0,2,1};
+    int arr[] = {0,2,1,3,1};
     int size = sizeof(arr)/sizeof(arr[0]);
 
-    vector<int>ans = countSmaller(arr,size);
+    vector<int>ans = countSmallerRight(arr,size);
 
     for(auto it : ans)
         cout<<it<<" ";
