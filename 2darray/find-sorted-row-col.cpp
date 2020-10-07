39ca93076fdffe2d2643163ee5bbc24b6c843934
aksharanigam1112 <aksharanigam111298@gmail.com>
2020-10-07 19:25:18 +0530

    Find element in sorted array

--- 2darray/find-sorted-row-col.cpp
+++ 2darray/find-sorted-row-col.cpp
@@ -1,3 +1,36 @@
+// Find element in sorted row and column
 #include<iostream>
-#include<bits/stdc++.h>
 using namespace std;
+#define n 4
+
+// TC : O(N) SC : O(1)
+void search(int mat[n][n], int x) 
+{ 
+	int i = 0, j = n-1;
+  
+    while (i<n && j >= 0) 
+    { 
+        if (mat[i][j] == x) 
+        { 
+            cout<<"Found at : ("<<i<<","<<j<<")"<<endl; 
+            return; 
+        } 
+        if (mat[i][j] > x) 
+            j--; 
+        else 
+            i++; 
+    } 
+  
+    cout<<"Element not found"<<endl; 
+        return;  
+} 
+
+int main() 
+{ 
+	int mat[n][n] = { { 10, 20, 30, 40 }, 
+					{ 15, 25, 35, 45 }, 
+					{ 27, 29, 37, 48 }, 
+					{ 32, 33, 39, 50 } }; 
+	search(mat,29); 
+} 
+
