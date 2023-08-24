e38c594bd2a9c921511ac6051949ee3dc4dd5331
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2023-08-24 23:57:11 +0530

    Adds brute force logic

--- graph/prims.cpp
+++ graph/prims.cpp
@@ -65,6 +65,49 @@ class Graph
 			}
 			print(dist);
 		} 
+
+		// BRUTE FORCE : 	TC : O(V^2) & SC : O(V)
+		void prims2(int src) {
+			vector<int> parent(this->V, -1), key(this->V, INT_MAX);
+			vector<bool> mst(this->V);
+
+			key[src] = 0;
+
+			for(int i=0;i<this->V;i++) {
+				int min = INT_MAX;
+				int u;
+
+				// 1. FIND MIN KEY NODE
+				for(int j=0;j<this->V;j++){
+
+					if(!mst[j] && key[j] < min){
+
+						min = key[j];
+						u = j;
+					}
+				}
+
+				// 2. MARK MIN KEY NODE = TRUE 
+				mst[u] = true;
+
+				// 3. FIND ITS ADJ ELES & UPDATE PARENT & KEYS ARRAYS
+				for(auto nbr : adj[u]) {
+					int v = nbr.first, w = nbr.second;
+
+					if(!mst[v] && w < key[v]) {
+						parent[v] = u;
+						key[v] = w;	
+					}
+				}
+
+			}
+
+			// Since array starts from 0 with parent -1, we will run from 1
+			for(int i=1;i<this->V;i++){
+				 cout<<parent[i]<<" -- "<<i<<" : "<<key[i]<<endl;
+			}
+
+		}
 };
 
 int main() 
@@ -73,22 +116,25 @@ int main()
 	
 	g.addEdge(0,1,15);
 	g.addEdge(0,2,12);
-    g.addEdge(0,4,10);
+    	g.addEdge(0,4,10);
 
 	g.addEdge(1,2,4);
 	g.addEdge(1,4,20);
-    g.addEdge(1,6,9);
-    g.addEdge(1,5,12);
+    	g.addEdge(1,6,9);
+    	g.addEdge(1,5,12);
 
 	g.addEdge(2,3,7);
 	g.addEdge(2,4,5);
 	
-    g.addEdge(3,5,5);
-    g.addEdge(3,4,2);
+    	g.addEdge(3,5,5);
+    	g.addEdge(3,4,2);
 	
-    g.addEdge(4,5,3);
-    g.addEdge(4,6,6);
+    	g.addEdge(4,5,3);
+    	g.addEdge(4,6,6);
 
+    	cout<<"Prims using min heap : "<<endl;
 	g.prims(0);
 
+	cout<<"Prims using without using min heap : "<<endl;
+	g.prims2(0);
 } 
