6879948d87412d8fd38187c4d86987f0d9286f7c
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2023-09-02 16:25:08 +0530

    Adds dfs logic

--- graph/bipartite.cpp
+++ graph/bipartite.cpp
@@ -9,44 +9,91 @@ using namespace std;
 
 class Graph {
 	private :
+	int nodes;
 	map<int, list<int>> adj;
-	map<int, int> visited;
+	vector<int> colours;
 
-	public:
-	Graph(vector<Pair>&edges) {
-		for(int i=0;i<edges.size();i++) {
-			int u = edges[i].first, v = edges[i].second;
-
-			adj[u].push_back(v);
-			adj[v].push_back(u);
-		}
-	}
-
-	bool isBipartiteBfs(int src) {
+	bool bfs(int src) {
 		queue<int> q;
 		q.push(src);
-		visited[src] = 1;
+		colours[src] = 0;
 
 		while(!q.empty()) {
 			int node = q.front();
 			q.pop();
 
 			for(int nbr : adj[node]) {
-				if(!visited[nbr]) {
+				
+				if(colours[nbr] == -1) {
 					q.push(nbr);
-					visited[nbr] = !visited[node];
+					colours[nbr] = !colours[node];
 				} 
 				// If nbr is already visited but has a different colour than what we are trying, 
 				// then it means two adjacent nodes have same colours, hence not bipartite
-				else {
-					int colour = !visited[node];
-					if(visited[nbr] != colour)
-						return false;
-				}
+				else if(colours[nbr] == colours[node])
+					return false;
 			}
 		}
 		return true;
 	}
+
+	bool dfs(int src) {
+		
+		for(int nbr : adj[src]) {
+
+			if(colours[nbr] == -1) {
+				colours[nbr] = !colours[src];
+				if (!dfs(nbr))
+					return false;
+			}
+
+			else if (colours[nbr] == colours[src])
+				return false;
+		}
+
+		return true;
+	}
+
+	public:
+	Graph(int n, vector<Pair>&edges) {
+		this->nodes = n;
+
+		for(int i=0;i<edges.size();i++) {
+			int u = edges[i].first, v = edges[i].second;
+
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+
+		colours = vector<int>(n, -1);
+	}
+
+	void clearColours() {
+		this->colours = vector<int>(this->nodes, -1);
+	}
+
+	bool isBipartiteBfs(int src) {
+		for(int i=0;i<this->nodes;i++) {
+	        if(colours[i] == -1) {
+	            if(bfs(i) == false)
+	                return false; 
+	        }
+	    }
+	    return true; 
+	}
+
+	bool isBipartiteDfs(int src) {
+		for(int i=0;i<this->nodes;i++) {
+	        if(colours[i] == -1) {
+	        	
+	        	colours[i] = 0;
+	            if(dfs(i) == false)
+	                return false; 
+	        }
+	    }
+	    return true; 
+	}
+
 }; 
 
 int main() {
@@ -55,14 +102,20 @@ int main() {
 		{3,4}, {4,5}, {5,6}, {5,8},
 		{6,7}, {8,9}
 	};
+	
+	Graph g = Graph(10, edges);
 
 	// vector<Pair> edges = {
 	// 	{0,1}, {1,2}, {2,3}, {2,10}, 
 	// 	{3,4}, {4,5}, {5,6}, {5,8},
 	// 	{6,7}, {7,10}, {8,9}
 	// };
-
-	Graph g = Graph(edges);
+	
+	// Graph g = Graph(11, edges);
 
 	cout<<"Is the Graph Bipartite using BFS : "<<(g.isBipartiteBfs(0) ? "Yes" : "No")<<endl;
+
+	g.clearColours();
+
+	cout<<"Is the Graph Bipartite using DFS : "<<(g.isBipartiteDfs(0) ? "Yes" : "No")<<endl;
 }
