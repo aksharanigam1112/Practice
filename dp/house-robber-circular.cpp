068e3e40c2961c5dcab9f090ddcc4852c6116306
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2023-09-09 23:02:11 +0530

    Adds optimized approach

--- dp/house-robber-circular.cpp
+++ dp/house-robber-circular.cpp
@@ -1,32 +1,106 @@
+// There are N houses built in a circular, each of which contains some value in it. 
+// A thief is going to steal the maximum value of these houses, but he can’t steal in two adjacent houses
+
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-   int rob(int nums[], int size) 
-    {
-        if(size==0)
-            return 0;
-        if(size==1)
-            return nums[0];
-        
-        if(size==2)
-            return max(nums[0],nums[1]);
-        
-    
-        int dp[size];
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        dp[2] = max(nums[0],nums[1]);
-        
-        for(int i=3;i<size;i++)
-        {
-            dp[i] = max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[size-1];
+
+// TC : O(2^N) 	& 	SC : O(N) 
+int recursion(vector<int>&arr, int index) {
+
+	if(index >= arr.size())
+		return 0;
+
+	return max(
+				arr[index] + recursion(arr, index+2), // include
+				recursion(arr, index+1) // exclude
+			);
+}
+
+// TC : O(N) 	& 	SC : O(N+N) 
+int topDown(vector<int>&arr, vector<int>&dp, int index) {
+
+	if(index >= arr.size())
+		return 0;
+
+	if(dp[index] != -1)
+		return dp[index];
+
+	dp[index] = max(arr[index] + topDown(arr, dp, index+2), topDown(arr, dp, index+1));
+	return dp[index];
+}
+
+
+// TC : O(N) 	& 	SC : O(N) 
+int bottomUp(vector<int>&arr) {
+	int n = arr.size();
+	if(n==0)
+		return 0;
+	
+	if(n==1)
+		return arr[0];
+
+	vector<int> dp(n, 0);
+
+	dp[0] = arr[0];
+    dp[1] = max(arr[1], arr[0]);
+
+    for(int i=2;i<n;i++) {
+
+        dp[i] = max(
+        	arr[i] + dp[i-2], // include
+        	dp[i-1] //exclude
+    	);
     }
 
-int main()
-{
-    int arr[] = {1,2,3,1};
-    int size = sizeof(arr)/sizeof(arr[0]);
+    return dp[n-1];
+}
 
-    cout<<"Max possible stealing : "<<rob(arr,size)<<endl;
+// TC : O(N) 	& 	SC : O(1) 
+int bottomUpOptimized(vector<int>&arr) {
+	int n = arr.size();
+	if (n == 0)
+        return 0;
+ 
+    int value1 = arr[0];
+    if (n == 1)
+        return value1;
+ 
+    int value2 = max(arr[0], arr[1]);
+    if (n == 2)
+        return value2;
+
+    int max_val;
+    
+    for (int i = 2; i < n; i++) {
+        max_val = max(arr[i] + value1, value2);
+        value1 = value2;
+        value2 = max_val;
+    }
+ 
+    return max_val;
+}
+
+int houseRobbery(vector<int>&arr) {
+	vector<int> temp1, temp2;
+
+	temp1.assign(arr.begin(), arr.end()-1);
+	temp2.assign(arr.begin()+1, arr.end());
+
+	return max(bottomUpOptimized(temp1), bottomUpOptimized(temp2));
+}
+
+int main() {
+	vector<int> arr = {2,1,3,7};
+
+	cout<<"Maximum possible stolen value when linear using recursion : "<<recursion(arr, 0)<<endl;
+
+	vector<int>dp(arr.size()+1, -1);
+	cout<<"Maximum possible stolen value when linear using Top Down : "<<topDown(arr, dp, 0)<<endl;
+
+	cout<<"Maximum possible stolen value when linear using Bottom Up : "<<bottomUp(arr)<<endl;
+
+	cout<<"Maximum possible stolen value when linear using Bottom Up optimized : "<<bottomUpOptimized(arr)<<endl;
+
+	cout<<"Maximum possible stolen value when circular :  "<<houseRobbery(arr)<<endl;
 }
