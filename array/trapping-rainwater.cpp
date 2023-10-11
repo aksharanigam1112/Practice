bd3c2eaa36617a70b7c66e904021a706fcc03470
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2023-10-11 20:30:17 +0530

    Adds optimized approach

--- array/trapping-rainwater.cpp
+++ array/trapping-rainwater.cpp
@@ -34,10 +34,35 @@ int level(int arr[], int size)
     return level;
 }
 
+// TC : O(N)    &   SC : O(1)
+int level2(int arr[], int size) {
+
+    int left=0, right=size-1, leftMax = 0, rightMax = 0, ans = 0;
+
+    while(left<=right) {
+        
+        if(leftMax >= rightMax) {
+            ans += max(0, rightMax - arr[right]);
+            rightMax = max(rightMax, arr[right]);
+            right--;
+        }
+
+        else {
+            ans += max(0, leftMax - arr[left]);
+            leftMax = max(leftMax, arr[left]);
+            left++;
+        }   
+    }
+
+    return ans;
+}
+
 
 int main()
 {
     int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
     int size = sizeof(arr)/sizeof(arr[0]);
+    
     cout<<"Level of water trapped : "<<level(arr,size)<<endl;
+    cout<<"Level of water trapped optimized : "<<level2(arr,size)<<endl;
 }
