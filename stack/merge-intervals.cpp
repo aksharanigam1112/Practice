27b4ad6a251a57fd35c452ab73f7598f34034d8a
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2023-11-06 23:45:33 +0530

    Adds space optimized approach

--- stack/merge-intervals.cpp
+++ stack/merge-intervals.cpp
@@ -3,51 +3,83 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-struct interval
-{
-    int start,end;
+
+struct Interval {
+    int start, end;
 };
 
-bool compare(interval &a, interval &b)
-{
-    return a.start < b.start ;
+// Sort ascending order
+bool compare(Interval p1, Interval p2) {
+    return p1.start < p2.start;
 }
 
-void print(stack<interval>&s)
-{
-    while(!s.empty())
-    {
-        cout<<"("<<s.top().start<<","<<s.top().end<<")"<<" ";
-        s.pop();
+void print(vector<Interval> arr){
+    for(Interval i : arr) {
+        cout<<"("<<i.start<<","<<i.end<<")";
     }
     cout<<endl;
 }
 
-void mergeIntervals(vector<interval>&arr)
-{
-    stack<interval>st;
-    
-    // Sorting the intervals based on their starting value
-    sort(arr.begin(),arr.end(),compare);
+// TC : O(N) & SC : 0(N)
+vector<Interval> mergeOverlappingInterval(vector<Interval> arr) {
+    if (arr.empty())
+        return vector<Interval>();
 
-    st.push(arr[0]);
+    sort(arr.begin(), arr.end(), compare);
 
-    for(int i=1;i<arr.size();i++)
-    {
-        interval it = arr[i];
+    stack<Interval> s;
+    s.push(arr[0]);
 
-        if(it.start > st.top().end)
-            st.push(it);
-        
-        if(st.top().end < it.end)
-            st.top().end = it.end; 
+    for(int i=1;i<arr.size();i++) {
+
+        // Current overlaps with top, then update top end time 
+        if(arr[i].start <= s.top().end)
+            s.top().end = max(s.top().end, arr[i].end);
+
+        else
+            s.push(arr[i]);
     }
-    print(st);
-}
+
+    vector<Interval>ans;
+    while(!s.empty()) {
+        ans.push_back(s.top());
+        s.pop();
+    }
+
+    return ans;
+} 
+
+// TC : O(N) & SC : 0(1)
+vector<Interval> mergeOverlappingInterval2(vector<Interval> arr) {
+    if (arr.empty())
+        return vector<Interval>();
+
+    sort(arr.begin(), arr.end(), compare);
+    
+    int prev = 0;
+
+    for(int i=1;i<arr.size();i++) {
+
+        // Current i overlaps with previous index
+        if(arr[i].start <= arr[prev].end)
+            arr[prev].end = max(arr[prev].end, arr[i].end);
+
+        else {
+            prev++;
+            arr[prev] = arr[i];
+        }
+    }
+
+    vector<Interval> ans(arr.begin(), arr.begin()+prev+1);
+    return ans;
+} 
 
 int main()
 {
-    vector<interval> arr =  {{1,5},{2,3},{4,6},{7,8},{8,10},{12,15}};
-    cout<<"Merged Intervals are : ";
-    mergeIntervals(arr);
+    vector<Interval> arr =  {{1,5},{2,3},{4,6},{7,8},{8,10},{12,15}};
+    
+    // vector<Interval> ans = mergeOverlappingInterval(arr);
+
+    vector<Interval> ans = mergeOverlappingInterval2(arr);
+    print(ans);
 }
