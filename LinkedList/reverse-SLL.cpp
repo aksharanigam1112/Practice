09ac36e65f19e9970b5a2ad1935dc39dc5101793
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2023-11-08 23:28:45 +0530

    Updates logic

--- LinkedList/reverse-SLL.cpp
+++ LinkedList/reverse-SLL.cpp
@@ -1,93 +1,112 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-class SLL
-{
-    public:
-    char data;
-    SLL *next;
+
+struct Node {
+    int data;
+    Node* next;
 };
 
-SLL *insert(SLL*head,char data)
-{
-    SLL*node  = new SLL();
-    node->data = data;
-    node->next = NULL;
-    if(head==NULL)
-        head = node;
-    else
-    {
-        node->next = head;
-        head = node;
+Node* newNode(int data){
+    Node *n = new Node;
+    n->data = data;
+    n->next = NULL;
+    return n;
+}
+
+void print(Node* head) {
+    
+    while(head != NULL) {
+        cout<<head->data<<" ";
+        head = head->next;
     }
+    cout<<endl;
+}
+
+// Recursive TC : O(N) & SC : O(N)
+Node* reverseRecur(Node* head) {
+
+    if(head == NULL || head->next == NULL)
+        return head;
+
+    Node* newHead = reverseRecur(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return newHead;
+}
+
+// Iterative TC : O(N) & SC : O(1)
+Node* reverse(Node* head) {
+    if(head == NULL || head->next == NULL)
+        return head;
+
+    Node* prev = NULL;
+
+    while(head->next != NULL) {
+        Node* temp = head->next;
+        head->next = prev;
+        prev = head;
+        head= temp;
+    }
+    head->next = prev;
     return head;
 }
 
-// Iterative Approach
 
-SLL* reverse(SLL*head)
-{
+Node* reverseBetween(Node* head, int left, int right) {
     if(head == NULL || head->next == NULL)
         return head;
-    SLL *prev = NULL , *curr = head , *nextpt ;
 
-    while(curr->next !=NULL)
-    {
-        nextpt = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = nextpt;
-    }
-    curr->next = prev;
-    return curr;
-}
-
-
-//Recursive Approach
-
-SLL* reverseRecur(SLL*head)
-{
-    if(head==NULL || head->next == NULL )
-        return head;
+    Node* prev = NULL, *temp1 = head, *temp2 = head;
     
-    SLL * newhead = reverseRecur(head->next);
-    head->next->next = head;
-    head->next = NULL;
-    return newhead;
+    int l = 1;
+    while(l < left) {
+        prev = temp1;
+        temp1 = temp1->next;
+        l++;
+    }
+
+    int r = 1;
+    while(r < right) {
+        temp2 = temp2->next;
+        r++;
+    }
+
+    Node* last = NULL;
+    if(temp2->next != NULL) {
+        last = temp2->next;
+        temp2->next = NULL; 
+    }
+
+    if(prev != NULL) {
+        prev->next = reverse(temp1);
+    } else {
+        head = reverse(temp1);
+    }
+
+    temp1->next = last; 
+    return head;
+    
 }
 
-int main()
-{
-    // SLL *head1;
-   
-    // head1 = insert(head1,'f');
-    // head1 = insert(head1,'e');
-    // head1 = insert(head1,'d');
-    // head1 = insert(head1,'c');
-    // head1 = insert(head1,'b');
-    // head1 = insert(head1,'a');
 
-    // SLL * rev = reverse(head1);
-    // while(rev!=NULL)
-    // {
-    //     cout<<rev->data<<" ";
-    //     rev = rev->next;
-    // }
+int main() {
+    Node* ll = newNode(10);
+    ll->next = newNode(20);
+    ll->next->next = newNode(30);
+    ll->next->next->next = newNode(40);
+    ll->next->next->next->next = newNode(50);
 
-    SLL *head2;
+    // cout<<"Reverse linked list using recursion : ";
+    // Node* newhead1 = reverseRecur(ll);
+    // print(newhead1);
 
-    head2 = insert(head2,'f');
-    head2 = insert(head2,'e');
-    head2 = insert(head2,'d');
-    head2 = insert(head2,'c');
-    head2 = insert(head2,'b');
-    head2 = insert(head2,'a');
+    // cout<<"Reverse linked list using iteration : ";
+    // Node* newhead2 = reverse(ll);
+    // print(newhead2);
 
-    SLL *newrev = reverseRecur(head2);
-    while(newrev!=NULL)
-    {
-        cout<<newrev->data<<" ";
-        newrev = newrev->next;
-    }
-    cout<<endl;
+    int left = 2, right = 4;
+    cout<<"Reverse linked list between nodes "<<left<<" to "<<right<<" : ";
+    Node* newhead3 = reverseBetween(ll, left, right);
+    print(newhead3);
 }
