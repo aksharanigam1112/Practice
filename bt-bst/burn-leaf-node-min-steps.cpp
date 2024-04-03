d373441dabe8f0bbf8d648dafc0db3eb5633f932
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2024-04-03 23:15:20 +0530

    Updates logic

--- bt-bst/burn-leaf-node-min-steps.cpp
+++ bt-bst/burn-leaf-node-min-steps.cpp
@@ -1,21 +1,56 @@
-// Minimum time to burn a Tree starting from a Leaf node
+// Minimum time to burn a Tree starting from a Leaf node.
+// Reference : https://www.geeksforgeeks.org/minimum-time-to-burn-a-tree-starting-from-a-leaf-node/
+
 #include<iostream>
-struct node
-{
+using namespace std;
+
+struct node{
     int data;
     node*left,*right;
 };
 
-node* newNode(int data)
-{
+struct RT {
+    int height, dist;
+
+    RT(int h, int d){
+        height = h;
+        dist = d;
+    }
+};
+
+node* newNode(int data){
     node*n = new node;
     n->data = data;
     n->left = n->right = NULL;
     return n;
 }
 
-int main()
-{
+
+int burnTime(node* root, int leaf, int &dist, int &ans){
+    if (root == NULL)
+        return 0;
+
+    if (root->data == leaf) {
+        dist = 0;
+        return 1;
+    }
+
+    int ldist = -1, rdist = -1;
+    int lh = burnTime(root->left, leaf, ldist, ans);
+    int rh = burnTime(root->right, leaf, rdist, ans);
+ 
+    if (ldist != -1) {
+        dist = ldist + 1;
+        ans = max(ans, dist + rh);
+    }
+    else if (rdist != -1) {
+        dist = rdist + 1;
+        ans = max(ans, dist + lh);
+    }
+    return max(lh, rh) + 1;
+}
+
+int main(){
     node* root = newNode(1); 
     root->left = newNode(2); 
     root->right = newNode(3); 
@@ -29,5 +64,8 @@ int main()
     root->left->right->right = newNode(10); 
     
     root->left->right->left->left = newNode(11); 
-    
+
+    int dist = -1, ans = 0;
+    burnTime(root, 11, dist, ans);
+    cout<<"Minimum time required to burn all nodes : "<<ans<<endl;
 }
