d7aaabc0852d11f2fa8c425c7a1b01b5f04e08d5
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2025-01-10 01:17:02 +0530

    Adds logic for corner cases

--- graph/rotten-tomato.cpp
+++ graph/rotten-tomato.cpp
@@ -1,92 +1,121 @@
 // Given some rotten tomatoes, find the no. of days all the tomatoes will get rotten.
 // Rotten tomoatoes only rot, in 4 directions (neighbours)
+// Ques : https://leetcode.com/problems/rotting-oranges/description/
+
 #include<iostream>
 #include<bits/stdc++.h>
-#define RS 4
-#define CS 5
 using namespace std;
+#define Pair pair<int,int>
 
-void findRotten(int arr[RS][CS], queue<int>&bfs,map<int,bool>&visited)
-{
-    for(int i=0;i<RS;i++)
-    {
-        for(int j=0;j<CS;j++)
-        {
-            if(arr[i][j]==0)
-            {
-                bfs.push(i*CS+j);
-                visited[i*CS+j]=true;
+class Graph {
+private: 
+    int RS, CS;  
+    vector<vector<int>> arr;
+    queue<Pair>q;
+    map<Pair, bool> visited;
+
+    void findRotten(){
+        
+        for(int i=0;i<RS;i++){
+            for(int j=0;j<CS;j++) {
+
+                // Check for arr[i][j] == 2 => Rotten orange
+                if(arr[i][j] == 2){
+                    q.push({i,j});
+                    visited[{i,j}] = true;
+                }
             }
         }
     }
-}
 
-int rotten(int arr[RS][CS])
-{
-    queue<int>bfs;
-    map<int,bool>visited;
+    bool isSafe(int i, int j){
+        // Check for arr[i][j] == 1 => Fresh orange
+        return i >= 0 && i < RS && j >= 0 && j < CS && arr[i][j] == 1 && !visited[{i,j}];
+    }
 
-    findRotten(arr,bfs,visited);
-    bfs.push(-1);
-    int day=0;
+    bool isAllRotten() {
 
-    while(!bfs.empty())
-    {
-        int ele = bfs.front();
-        bfs.pop();
-
-        if(ele==-1)
-        {
-            if(!bfs.empty())
-            {
-                bfs.push(-1);
-                day++;
-            } 
-        }
-        else
-        {
-            int R = ele/CS, C=ele%CS;
+        for(int i=0; i<RS; i++) {
+            for(int j=0; j<CS; j++) {
                 
-            // UP
-            if(R-1>=0 && arr[R-1][C]!=0 && visited.find((R-1)*CS+C)==visited.end())
-            {
-                bfs.push((R-1)*CS+C);
-                visited[(R-1)*CS+C] = true;
-            }
-
-            // DOWN
-            if(R+1<RS && arr[R+1][C]!=0 && visited.find((R+1)*CS+C)==visited.end())
-            {
-                bfs.push((R+1)*CS+C);
-                visited[(R+1)*CS+C] = true;
-            }
-
-            // LEFT 
-            if(C-1>=0 && arr[R][C-1]!=0 && visited.find(R*CS+(C-1))==visited.end())
-            {
-                bfs.push(R*CS+(C-1));
-                visited[R*CS+(C-1)] = true;
-            }
-
-            //RIGHT
-            if(C+1<CS && arr[R][C+1]!=0 && visited.find(R*CS+(C+1))==visited.end())
-            {
-                bfs.push(R*CS+(C+1));
-                visited[R*CS+(C+1)] = true;
+                // Non-empty cell & not visited => Fresh orange exists
+                if(arr[i][j] == 1 && !visited[{i,j}])
+                    return false;
             }
         }
+
+        return true;
     }
-    return day;
 
-}
+public:
 
-int main()
-{
-    int arr[RS][CS];
-    memset(arr,1,sizeof(arr));
-    arr[0][0]=0;
-    arr[1][1]=0;
-    arr[2][4]=0;
-    arr[3][0]=0;
-    cout<<"No. of days required : "<<rotten(arr)<<endl;
+    Graph(vector<vector<int>> arr){
+        this->arr = arr;
+        this->RS = arr.size();
+        this->CS = arr[0].size();
+    }
+
+    int getDaysToRot(){
+        
+        findRotten();
+        // This acts as a marker to count no of days elapsed in rotting
+        q.push({-1,-1});
+        int days = 0;
+
+        while(!q.empty()){
+            auto [R,C] = q.front();
+            q.pop();
+            // cout<<"Element popped : "<<R<<","<<C<<endl;
+
+            if(R == -1 && C == -1 && !q.empty()){
+                q.push({-1,-1});
+                days++;
+            }
+
+            else {
+                // UP
+                if(isSafe(R-1, C)){
+                    q.push({R-1,C});
+                    visited[{R-1,C}] = true;
+                }
+
+                // DOWN
+                if(isSafe(R+1, C)){
+                    q.push({R+1,C});
+                    visited[{R+1,C}] = true;
+                }
+
+                // LEFT 
+                if(isSafe(R, C-1)){
+                    q.push({R,C-1});
+                    visited[{R,C-1}] = true;
+                }
+
+                // RIGHT
+                if(isSafe(R, C+1)){
+                    q.push({R,C+1});
+                    visited[{R,C+1}] = true;
+                }
+            }
+        }
+
+        return isAllRotten() ? days : -1; 
+    }
+};
+
+
+int main(){
+    vector<vector<int>> arr = {
+        {2,1,1},{1,1,0},{0,1,1}
+    }; // Ans : 4
+
+    // vector<vector<int>> arr = {
+    //     {2,1,1},{0,1,1},{1,0,1}
+    // }; // Ans : -1
+
+    // vector<vector<int>> arr = {{0,2}}; // Ans : 0
+
+    Graph g(arr);
+
+    cout<<"No. of days taken to rot : "<<g.getDaysToRot()<<endl;
 }
