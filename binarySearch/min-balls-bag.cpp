7ca3be703ae9994e66762ccbc030f59d8ce2a963
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2025-01-13 00:27:20 +0530

    Adds ques link

--- binarySearch/min-balls-bag.cpp
+++ binarySearch/min-balls-bag.cpp
@@ -1,35 +1,40 @@
 // 1760 Leetcode Medium
+// Ques : https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/description/
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
-int minimumSize(vector<int>& nums, int maxOperations) 
-{
-    int start=1,end = *max_element(nums.begin(),nums.end()),mid,ans;
+int minimumSize(vector<int>& nums, int maxOperations) {
+    int start=1, end = *max_element(nums.begin(),nums.end()), ans;
 
     while(start<=end)
     {
-        mid = (start+end)/2;
-        int op=0;
-        for(int i=0;i<nums.size();i++)
-            op+= (nums[i]+mid-1)/mid - 1;
+        int mid = (start+end)/2, op=0;
+
+        // Loop through all the bags to check for operations required 
+        // to reduce the penalty score.
+        for(int i=0; i<nums.size(); i++)
+            op += nums[i]/mid - 1;
         
-        if(op<=maxOperations)
-        {
+        if(op <= maxOperations){
             ans = mid;
             end = mid-1;
         }
         else
             start = mid+1;
     }
+
     return ans;
 }
 
 
 int main()
 {
-    vector<int>nums = {7,17};
-    int maxOperations = 2;
+    // vector<int>nums = {7,17}; int maxOperations = 2; // Ans : 5
+
+    // vector<int> nums = {2,4,8,2}; int maxOperations = 4; // Ans : 2
+
+    vector<int> nums = {9}; int maxOperations = 2; // Ans : 3
 
     cout<<"Min Penalty : "<<minimumSize(nums,maxOperations)<<endl;
 }   
